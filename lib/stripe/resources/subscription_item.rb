d12214602217bd1c01d244bd7c6d72273593d4ce
Felipe Lima <felipe.lima@gmail.com>
2022-01-13 18:04:53 -0300

    define custom_method

--- lib/stripe/resources/subscription_item.rb
+++ lib/stripe/resources/subscription_item.rb
@@ -11,14 +11,19 @@ module Stripe
 
     OBJECT_NAME = "subscription_item"
 
+    custom_method :usage_records, http_verb: :get, http_path: "usage_records"
     nested_resource_class_methods :usage_record, operations: %i[create]
     nested_resource_class_methods :usage_record_summary,
                                   operations: %i[list],
                                   resource_plural: "usage_record_summaries"
 
     def usage_records(params = {}, opts = {})
-      resp, opts = execute_resource_request(:get, resource_url + "/usage_records", params, opts)
-      Util.convert_to_stripe_object(resp.data, opts)
+      request_stripe_object(
+        method: :get,
+        path: resource_url + "/usage_records",
+        params: params,
+        opts: opts
+      )
     end
 
     def usage_record_summaries(params = {}, opts = {})
